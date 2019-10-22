bf7c64984ae8a0f6e95d4bdc96f6e5c430fdf9e7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:50:33 +0200

    creazione del 5 esercizio

---
+++ Gestione_Thread/Thread5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workerThread = new Thread(() =>
+            {
+                  Console.WriteLine("inizio di un thread molto lungo");
+                  Thread.Sleep(500);
+                  Console.WriteLine("Termine worker thread");
+  
+            });
+
+            workerThread.Start();
+            workerThread.Join(500);
+
+            if(workerThread.ThreadState!=ThreadState.Stopped)
+            {
+
+            }
+        }
+    }
+}
